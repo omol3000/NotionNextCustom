b568ec1d8dfbd77f8240e7e194cd505abd4dab78
Niels <niels@growhuman.io>
2026-01-03 19:42:26 +0100

    Add notice to footer, change geo defaults to DE

--- themes/nobelst/components/Footer.js
+++ themes/nobelst/components/Footer.js
@@ -1,11 +1,14 @@
 import DarkModeButton from '@/components/DarkModeButton'
 import Vercel from '@/components/Vercel'
 import { siteConfig } from '@/lib/config'
+import dynamic from 'next/dynamic'
+
+const NotionPage = dynamic(() => import('@/components/NotionPage'))
 
 export const Footer = (props) => {
   const d = new Date()
   const currentYear = d.getFullYear()
-  const { post } = props
+  const { post, notice } = props
   const fullWidth = post?.fullWidth ?? false
   const since = siteConfig('SINCE')
   const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
@@ -16,6 +19,14 @@ export const Footer = (props) => {
      }`}
    >
      <DarkModeButton className='text-center py-4'/>
+     
+     {/* Notice aus Notion */}
+     {notice?.blockMap && (
+       <div className="my-6 p-4 bg-gray-50 dark:bg-gray-800 rounded-lg">
+         <NotionPage post={notice} className='text-sm' />
+       </div>
+     )}
+     
      <hr className="border-gray-200 dark:border-gray-600" />
      <div className="my-4 text-sm leading-6">
        <div className="flex align-baseline justify-between flex-wrap">
